9b4dbf726946b749aa29a99d00074610d46e6262
LoganWatson1400 <glass@LegendayLaptop.>
2026-01-31 14:07:22 -0600

    feat: bang

---
+++ src/routes/+page.svelte
@@ -0,0 +1,46 @@
+<!--- bout pg --->
+
+<!--- Title --->
+<div
+  class="card"
+  style="
+  	margin: auto; 
+  	justify-content: center; 
+  	width: fit-content;"
+>
+  <h1 style="padding: 16px;">Hello World!</h1>
+</div>
+
+<!--- bout --->
+<div class="card" style="width: 50%; margin: auto; justify-content: center;">
+  <h3 style="padding: 16px;">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit, ut omnis
+    similique commodi nostrum, numquam, dignissimos autem reprehenderit beatae
+    aut in dolorem totam! Officiis et similique architecto fugit nihil porro.
+  </h3>
+
+  <!--- btn --->
+  <div style="display: flex; flex-wrap: wrap; gap: 8px 32px; padding: 32px; padding-top: 0px;">
+    <a
+      href="https://github.com/LoganWatson1400"
+      target="_blank"
+      style="flex: 1;"
+    >
+      <button class="btn" title="github">Github</button>
+    </a>
+    <a
+      href="https://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=&cad=rja&uact=8&ved=2ahUKEwju_an3wLaSAxUG5ckDHfcOG20QwqsBegQIDBAB&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DdQw4w9WgXcQ&usg=AOvVaw0aHtehaphMhOCAkCydRLZU&opi=89978449"
+      target="_blank"
+      style="flex: 2;"
+    >
+      <button class="btn" title="resume">Resume</button>
+    </a>
+    <a
+      href="https://linkedin.com/in/logan-watson-bbb14b278"
+      target="_blank"
+      style="flex: 1;"
+    >
+      <button class="btn" title="linkedin">LinkedIn</button>
+    </a>
+  </div>
+</div>
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [sveltekit()],
+  server: {
+    watch: {
+      usePolling: true,
+    }
+  }
+})
