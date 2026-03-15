a4f74bb103e5f4667598f493e11c09e69857cac6
loganWatson1400 <21watsol@gmail.com>
2026-03-15 14:57:50 -0500

    feat: added all current projects

--- src/routes/projects/[slug]/+page.svelte
+++ src/routes/projects/[slug]/+page.svelte
@@ -1,60 +1,68 @@
 <script>
   export let data;
-  /*
-  * "title": "E-Commerce Platforma",
-  * "route": "ecommerceplatform",
-  * "description": "A full-stack online shopping platform with payment integration and inventory management.Lorem ipsuLorem ipsum, dolor sit amet consectetur adipisicing elit. Unde beatae, iure aliquid odio id ratione, ullam deleniti nam maiores quas vitae aut adipisciipsam earum commodi officia necessitatm, dolor sit amet consectetur adipisicing elit. Unde beatae, iure aliquid odio id ratione, ullam deleniti nam maiores quas vitae aut adipisciipsam earum commodi officia necessitatibus sint. Iusto!",
-  * "image": "/img/tmp.png",
-  * "image_alt": "E-commerce Platform Img",
-  * "tags": ["React", "Node.js", "MongoDB"],
-  * "link": "ecommerce"
-  */
 </script>
 
-<div id="Project Container"
-  style="
-    display: flex;
-    flex-direction: column;
-    gap: 16px;
-">
-  <div id="Header" 
-    style="
-      display: flex;
-      align-items: center;
-      gap: 8px;
-  ">
-    <h1 style="flex: 1 1 auto; text-align: center;">{data.project.title}</h1>
-  
+<div class="flex col project-container">
+  <div class="flex header" style="align-items:center;">
+    <h1 class="project-title" style="flex:1; margin:0;">
+      {data.project.title}
+    </h1>
     {#if data.project.link}
-      <a href={data.project.link} target="_blank" rel="noopener noreferrer" style="flex: 0 1 auto; display: flex;">
-        <img src="/img/github.svg" alt="GitHub" style="width: 32px; height: 32px;" />
+      <a
+        href={data.project.link}
+        target="_blank"
+        rel="noopener noreferrer"
+        class="github-link"
+      >
+        <img src="/img/github.svg" alt="GitHub" />
       </a>
     {/if}
-
-    <button class="btn"
-      style="
-        flex: 0 1 auto;
-        width: fit-content;
-      ">
+    <button class="btn" style="max-width: 64px;">
       <a href="/projects">back</a>
     </button>
   </div>
+
   <hr class="divider white" />
-  {#if data.project.image == ""}
-    <h3
-      style="
-      color: var(--color-txt-secondary); 
-      padding: 20px;
-      text-align: center;
-    ">
-      No Image Available
-    </h3>
-  {:else}
-    <img
-      src={data.project.image}
-      alt={data.project.image_alt}
-      style="width: 100%; padding: auto;">
-  {/if}
-  <hr class="divider white" />
-  <p style="font-size: 32px; padding: 0px 32px;">{data.project.description}</p>
+
+  <div class="project-content">
+    {#if data.project.image === ""}
+      <h3 class="no-image">No Image Available</h3>
+    {:else}
+      <img
+        class="project-image"
+        src={data.project.image}
+        alt={data.project.image_alt}
+      />
+    {/if}
+
+    <!-- <hr class="divider white content-divider" /> -->
+
+    <p class="project-description">
+      {data.project.description}
+    </p>
+  </div>
 </div>
+
+<style>
+  .project-content {
+    container-type: inline-size;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 24px;
+    align-items: start;
+    justify-items: center;
+  }
+
+  .project-image {
+    flex: 1 1 auto;
+    min-width: 50%;
+  }
+
+  .project-description {
+    flex: 2 1 300px;
+    min-width: 0;
+    border: 2px solid white;
+    padding: 16px;
+    font-size: 32px;
+  }
+</style>
